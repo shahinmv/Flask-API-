56a27423583dce4875e5c81ba45d1d21402a1295
Shahin Mammadov <ShahinMammadov@embawood.dm>
2022-03-01 16:07:44 +0400

    Added postgresql

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
@@ -10,10 +12,14 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vujxcwpvxtfbcy:1b97474a288c7d7514c1e79a4a843595a39cf510d5ff982fc7906c5d850527ad@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d61n678lulfp67'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
